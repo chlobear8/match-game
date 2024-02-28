fcf3e678da93d43c173071d4a460208a56fae36b
Chloe ONeil <chloesporri@yahoo.com>
2024-02-28 11:26:02 -0800

    add cardArray and createBoard function to app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+    name: 'dog',
+    img: 'imgs/dog.png'
+    },
+    {
+      name: 'cat',
+      img: 'imgs/cat.png'
+    },
+    {
+      name: 'fish',
+      img: 'imgs/fish.png'
+    },
+    {
+      name: 'bird',
+      img: 'imgs/bird.png'
+    },
+    {
+      name: 'squirrel',
+      img: 'imgs/squirrel.png'
+    },
+    {
+      name: 'turtle',
+      img: 'imgs/turtle.png'
+    },
+  ]
+
+  const grid = document.querySelector('.grid')
+
+  //create board
+  function createBoard() {
+    for (let i=0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'imgs/block.png')
+      card.setAttribute('data-id', i)
+      //card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+createBoard()
+})
